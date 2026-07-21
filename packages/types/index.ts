b6cbf9ce5d6e4b6129704db1ab06131b4c0bd909
MohamedFazil1406 <mohamedfazil01406@gmail.com>
2026-07-21 14:55:58 +0530

    refractor: changes the tsconfig setting to build the dist folder.

---
+++ packages/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./src/message";
+export * from "./src/operation";
+export * from "./src/pointer";
+export * from "./src/scroll";
+export * from "./src/terminal";
+export * from "./src/socket-event";
